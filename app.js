9b62fb1652104c3ab8aeb43c175fec5b555ee55e
Gerardo <kalicech@gmail.com>
2022-03-17 18:46:23 -0600

    segundo paso hacia paquetería inquirer

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ require("colors");
 const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 const main = async () => {
-  console.clear();
   console.log("hola");
-  mostrarMenu();
-  pausa()
+  let opt = "";
+  do {
+    //espera hasta que tengamos una respuesta
+    opt = await mostrarMenu();
+    console.log({ opt });
+    if(opt !== '0') await pausa();
+  } while (opt !== "0");
 };
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,43 +1,52 @@
+const { rejects } = require("assert");
+const { resolve } = require("path");
+
 require("colors");
 
 const mostrarMenu = () => {
-  console.clear();
-  console.log("==============".green);
-  console.log("seleccione una opcion".green);
-  console.log("==============\n".green);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log("==============".green);
+    console.log("seleccione una opcion".green);
+    console.log("==============\n".green);
 
-  console.log(`${"1".green}. crear una tarea`);
-  console.log(`${"2".green}. listar tareas`);
-  console.log(`${"3".green}. listar tareas completadas`);
-  console.log(`${"4".green}. listar tareas pendientes`);
-  console.log(`${"5".green}. completar tarea(s)`);
-  console.log(`${"6".green}. borrar tarea`);
-  console.log(`${"0".green}. salir`);
+    console.log(`${"1".green}. crear una tarea`);
+    console.log(`${"2".green}. listar tareas`);
+    console.log(`${"3".green}. listar tareas completadas`);
+    console.log(`${"4".green}. listar tareas pendientes`);
+    console.log(`${"5".green}. completar tarea(s)`);
+    console.log(`${"6".green}. borrar tarea`);
+    console.log(`${"0".green}. salir`);
 
-  //creación de interfaz para recibir y entregar información
-  const readLine = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    //creación de interfaz para recibir y entregar información
+    const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readLine.question("Seleccione una opcion: ", (opt) => {
-    readLine.close();
+    readLine.question("Seleccione una opcion: ", (opt) => {
+      readLine.close();
+      resolve(opt);
+    });
   });
 };
 
 const pausa = () => {
-  const readLine = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const readLine = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readLine.question(`\nPresione ${'ENTER'.green} para continuar`, (opt) => {
-    readLine.close();
+    readLine.question(`\nPresione ${"ENTER".green} para continuar`, (opt) => {
+      readLine.close();
+      resolve();
+    });
   });
 };
 
 //exportamos todos las funciones
 module.exports = {
   mostrarMenu,
-  pausa
+  pausa,
 };
